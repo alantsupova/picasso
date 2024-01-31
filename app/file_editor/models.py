3a1d06992a75cb6a8d3d0ebf719202fb27e4e61e
aleksandra <aleksaantsupova@yandex.ru>
2024-01-31 17:25:37 +0300

    change database and create model

--- app/file_editor/models.py
+++ app/file_editor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Files(models.Model):
+    files = models.FileField('Файл', upload_to='files/')
+    uploaded_at = models.DateTimeField('Дата и время загрузки', auto_now_add=True)
+    processed = models.BooleanField('Обработка', default=False)
+
+    def __str__(self):
+        return f'Файл {self.id}'
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
